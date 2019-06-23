9b51393e1a5126c6b16d78e6ad6b6c8f51865e34
Nick <nick_raston@hotmail.com>
2019-06-24 03:17:33 +1200

    Added basic toolbar, changed app icons, updated gradle.

--- app/src/main/java/com/vogella/android/eventlistenerapp/MainActivity.java
+++ app/src/main/java/com/vogella/android/eventlistenerapp/MainActivity.java
@@ -1,16 +1,26 @@
 package com.vogella.android.eventlistenerapp;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
+
+    }
+
+    private void setSupportActionBar(Toolbar myToolbar) {
     }
 
 }
