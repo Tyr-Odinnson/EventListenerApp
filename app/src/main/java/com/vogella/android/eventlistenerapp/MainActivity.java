c46c8c80e1ebba856a842b510b896d7e78553fd3
Tyr-Odinnson <50427951+Tyr-Odinnson@users.noreply.github.com>
2019-06-24 15:48:16 +1200

    Toolbars DONE!!!!

--- app/src/main/java/com/vogella/android/eventlistenerapp/MainActivity.java
+++ app/src/main/java/com/vogella/android/eventlistenerapp/MainActivity.java
@@ -14,13 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        setSupportActionBar(myToolbar);
-
 
     }
 
-    private void setSupportActionBar(Toolbar myToolbar) {
-    }
 
 }
